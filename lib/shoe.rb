b0d3fa8759e55a5e5b19acf0dba4873d5c65ef3f
Geoff Elliott <tinywv@gmail.com>
2019-10-19 01:04:30 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,18 @@
 # Make your shoe class here!
+class Shoe
+
+  #Create Arrtibute accessors
+  attr_accessor :brand, :color, :size, :material, :condition
+
+  #Define the initialize method
+  def initialize(brand)
+    @brand = brand
+  end
+
+  #Define the cobble method and change shoe condition(instance variable)
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
+
+end
